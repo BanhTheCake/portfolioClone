37fde78222753fed31fdcb6f4e53596cb3568950
Binhanh <binhanh080103@gmail.com>
2022-08-25 18:46:47 +0700

    add React.lazy for web

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,33 @@
 import './App.scss'
-import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+// import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 import { Navbar } from './components'
 import Social from './components/Social/Social';
-import { useEffect } from 'react';
+import React, { Suspense, useCallback, useEffect } from 'react';
 import { useState } from 'react';
-import Test from './container/Test/Test';
+
+const About = React.lazy(() => import('./container/About/About'));
+const Footer = React.lazy(() => import('./container/Footer/Footer'));
+const Header = React.lazy(() => import('./container/Header/Header'));
+const Skills = React.lazy(() => import('./container/Skills/Skills'));
+const Testimonial = React.lazy(() => import('./container/Testimonial/Testimonial'));
+const Work = React.lazy(() => import('./container/Work/Work'));
 
 function App() {
 
-
-    return (
-        <div className="app">
-                <Navbar />
-                <Header />
-                <About />
-                <Work />
-                <Skills />
-                <Testimonial />
-                <Footer />
-                <Social />
-        </div>
-    );
+  return (
+      <div className="app">
+        <Suspense fallback={<div>Loading...</div>}>
+              <Navbar />
+              <Header />
+              <About />
+              <Work />
+              <Skills />
+              <Testimonial />
+              <Footer />
+              <Social />
+        </Suspense>
+      </div>
+  );
 }
 
 export default App;
